90463eff3488b2b1744d45fd5af052670d39a93b
Mike Schore <mike.schore@gmail.com>
2021-06-29 20:21:50 +0800

    api: expose explicit buffer management option for http streams
    Signed-off-by: Mike Schore <mike.schore@gmail.com>

--- library/common/http/client.cc
+++ library/common/http/client.cc
@@ -176,9 +176,10 @@ void Client::DirectStream::resetStream(StreamResetReason reason) {
   callbacks_->onError();
 }
 
-void Client::startStream(envoy_stream_t new_stream_handle, envoy_http_callbacks bridge_callbacks) {
+void Client::startStream(envoy_stream_t new_stream_handle, envoy_http_callbacks bridge_callbacks, bool explicit_buffering) {
   ASSERT(dispatcher_.isThreadSafe());
   Client::DirectStreamSharedPtr direct_stream{new DirectStream(new_stream_handle, *this)};
+  direct_stream->explicit_buffering_ = explicit_buffering;
   direct_stream->callbacks_ =
       std::make_unique<DirectStreamCallbacks>(*direct_stream, bridge_callbacks, *this);
 
